6132af9ae2c1af93a54ccdc88a80b3abdefa64b0
trymycode <imitasingha1994@gmail.com>
2019-06-30 18:31:25 +0530

    Initialize Blog Component

---
+++ src/components/BlogComponent.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+class BlogComponent extends Component {
+  render() {
+    return (
+      <div>
+        from blog com
+      </div>
+    )
+  }
+}
+export default BlogComponent;
