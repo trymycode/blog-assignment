1cb0ed80634399fb45318790d99edeabb3b68677
trymycode <imitasingha1994@gmail.com>
2019-06-30 20:20:34 +0530

    Fetched data

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -1,7 +1,19 @@
 import React, { Component } from 'react'
 import BlogComponent from './BlogComponent';
+import axios from 'axios';
 
 class HomeComponent extends Component {
+
+  componentDidMount() {
+    axios.get(`http://test.peppersquare.com/api/v1/article`)
+    .then(res => {
+      const blogs = res.data;
+      this.setState({
+        blogs: blogs
+      });
+    })
+  }
+
   render() {
     return (
       <div>
